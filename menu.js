53025c76a4e29bd078cf45e72fc113a5fc0e565f
PeterWagon <74815335+PeterWagon@users.noreply.github.com>
2026-04-30 17:43:14 +0100

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,16 @@
+const menu = [
+    { name: "鸡肉粟米汤", price: 4.2 },
+    { name: "蟹肉粟米汤", price: 4.2 },
+    { name: "酸辣汤", price: 4.2 },
+    { name: "鸡丝面汤", price: 4.6 },
+
+
+// 你可以继续加很多（500个也没问题）
+    { name: "鸡丝蘑菇汤", price: 4.2 },
+    { name: "紫菜海鲜汤", price: 5.1 },
+    { name: "鸭丝汤", price: 5.1 },
+    { name: "云吞汤", price: 5.1 },
+    { name: "云吞面汤", price: 5.1 }
+
+];
+
